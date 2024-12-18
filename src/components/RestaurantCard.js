0f04a74ba1f06e9be7536b885460584e2cd05971
Sudheerakkireddy <sudheerakkireddy@gmail.com>
2024-12-18 15:53:35 +0530

    Refactoring code

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,27 +1,30 @@
 import { CDN_URL } from "../utils/constants";
+
 const RestaurantCard = (props) => {
   const { resData } = props;
-  const {
-    cloudinaryImageId,
-    name,
-    avgRating,
-    cuisines,
-    costForTwo,
-    deliveryTime,
-  } = resData?.data;
+
+  const { cloudinaryImageId, name, cuisines, avgRating, costForTwo } =
+    resData?.info;
+  const { deliveryTime } = resData?.info?.sla;
   return (
-    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+    <div
+      className="restaurant-card"
+      style={{
+        backgroundColor: "#f0f0f0",
+      }}
+    >
       <img
-        className="res-logo"
-        alt="res-logo"
+        className="restaurant-logo"
         src={CDN_URL + cloudinaryImageId}
+        alt="restaurant-logo"
       />
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} stars</h4>
-      <h4>₹{costForTwo / 100} FOR TWO</h4>
-      <h4>{deliveryTime} minutes</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{deliveryTime} Minutes</h4>
     </div>
   );
 };
+
 export default RestaurantCard;
